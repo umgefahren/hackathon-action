ffcd29fc061405e099fc2b6f82bb80e370d21c4e
umgefahren <55623006+umgefahren@users.noreply.github.com>
2022-10-13 19:33:37 +0200

    Basic cchhanges

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 
-import { DateTime } from 'luxon'
+import {DateTime} from 'luxon'
 
 async function run(): Promise<void> {
   try {
-
     const myToken = core.getInput('my_token')
 
     const octokit = github.getOctokit(myToken)
 
-    const { owner, repo } = github.context.repo
-
+    const {owner, repo} = github.context.repo
 
     const issue_number_string: string = core.getInput('issue_number')
     const issue_number = parseInt(issue_number_string)
@@ -20,12 +18,12 @@ async function run(): Promise<void> {
       owner,
       repo,
       issue_number,
-      body: "Action comment"
+      body: 'Action comment'
     })
 
     core.debug(`Association: ${comment.data.author_association}`)
     core.debug(`User: ${comment.data.user}`)
-    
+
     /*
     const comments = await octokit.rest.issues.listComments({
       owner,
@@ -36,13 +34,10 @@ async function run(): Promise<void> {
 
     // comments.data.find((comment) => comment.user.)
 
-
     const hackathon_end_string: string = core.getInput('hackathon_end')
     const hackathon_end = DateTime.fromISO(hackathon_end_string)
 
-
     const diff = hackathon_end.diffNow()
-    
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
